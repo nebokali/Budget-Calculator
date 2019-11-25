2e95d34b7a034cf01dfa948ef7e3d5fd31a87c0f
nebokali <nebokali@gmail.com>
2019-11-25 17:29:40 +0300

    Изменения
    Добавлен краткий путь для подсчетов

--- index.js
+++ index.js
@@ -3,45 +3,89 @@
 let money = prompt('Ваш бюджет на месяц?'),
     time = prompt('Введите дату в формате YYYY-MM-DD');
 
-function changeData(days) {
-  var appData = {
-    //бюджет
-    budjet: money/30*days, 
-    //данные времени
-    timeData: time,
-    //объект с обязательными расходами
-    expenses: {},
-    //объект с необязательными расходами
-    optionalExpenses: {},
-    //массив данных с доп. доходом
-    income: [],
-    //свойство savings (выставляем его как false)
-    savings: false
-  }
+// function changeData(days) {
+//   var appData = {
+//     //бюджет
+//     budjet: money/30*days,
+//     //данные времени
+//     timeData: time,
+//     //объект с обязательными расходами
+//     expenses: {},
+//     //объект с необязательными расходами
+//     optionalExpenses: {},
+//     //массив данных с доп. доходом
+//     income: [],
+//     //свойство savings (выставляем его как false)
+//     savings: false
+//   };
+//
+//   //заполнение объекта обязательных расходов
+//   var expensesName = '';
+//   var expensesCost = 0;
+//   var budgetLimit = appData.budjet;
+//
+//   function setExpenses() {
+//     expensesName = prompt('Введите обязательную статью расходов в этом месяце');
+//     expensesCost = prompt('Во сколько обойдется?')/30*days;
+//     var newExpenses = { [expensesName]: expensesCost };
+//     var oldExpenses = appData.expenses;
+//     //учитываем обязательные расходы в бюджете на период
+//     budgetLimit = budgetLimit - expensesCost;
+//     appData.expenses = Object.assign(oldExpenses, newExpenses);
+//     return budgetLimit;
+//   }
+//
+//   //дважды запрашиваем информацию по обяхательным расходам
+//   for (var i = 0; i < 2; i++) {
+//     setExpenses();
+//   }
+//
+//   alert("Ваш бюджет на " + days + " день составляет: " + appData.budjet + ".  Если вычесть обязательные расходы, то за " + days + " день вы можете потратить: " + budgetLimit + ".");
+// }
+//
+// //выводим на экран бюджет на 1 день
+// changeData(1);
 
-  //заполнение объекта обязательных расходов
-  var expensesName = '';
-  var expensesCost = 0;
-  var budgetLimit = appData.budjet;
+//Второй путь
 
-  function setExpenses() {
-    expensesName = prompt('Введите обязательную статью расходов в этом месяце');
-    expensesCost = prompt('Во сколько обойдется?')/30*days;
-    var newExpenses = { [expensesName]: expensesCost };
-    var oldExpenses = appData.expenses;
-    //учитываем обязательные расходы в бюджете на период
-    budgetLimit = budgetLimit - expensesCost;
-    appData.expenses = Object.assign(oldExpenses, newExpenses);
-    return budgetLimit;
-  }
+let appData = {
+     //бюджет
+     budjet: money,
+     //данные времени
+     timeData: time,
+     //объект с обязательными расходами
+     expenses: {},
+     //объект с необязательными расходами
+     optionalExpenses: {},
+     //массив данных с доп. доходом
+     income: [],
+     //свойство savings (выставляем его как false)
+     savings: false
+};
 
-  //дважды запрашиваем информацию по обяхательным расходам
-  for (var i = 0; i < 2; i++) {
-    setExpenses();
-  }
+//Цикл для вопросов
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
 
-  alert("Ваш бюджет на " + days + " день составляет: " + appData.budjet + ".  Если вычесть обязательные расходы, то за " + days + " день вы можете потратить: " + budgetLimit + ".");
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.lenght <= 50) {
+      appData.expenses[a] = b; // создание пар ключ - значение
+    } else {
+      console.log("Ошибка");
+    }
 }
 
-//выводим на экран бюджет на 1 день
-changeData(1);
+appData.moneyPerDay  = appData.budjet / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Ошибка");
+}
